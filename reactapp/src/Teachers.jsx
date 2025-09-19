238615a7aab1e0850339403f20892d593809a25c
MrMancs <marcell.szatmari2007@gmail.com>
2025-09-19 10:59:43 +0200

    Add Teachers component

---
+++ reactapp/src/Teachers.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class Teachers extends React.Component {
+    state = {
+        teachers: []
+    }
+
+  render() {
+    return(
+      <div style={{marginLeft: "20px"}}>
+        {JSON.stringify(this.state.teachers)}
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    //TODO callout for teachers data
+  }
+}
