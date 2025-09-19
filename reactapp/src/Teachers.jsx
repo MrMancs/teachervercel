04573eb14f0b5ae0d41e0f2d10f9a0c87a704af3
MrMancs <marcell.szatmari2007@gmail.com>
2025-09-19 12:41:32 +0200

    Fix callout URL

--- reactapp/src/Teachers.jsx
+++ reactapp/src/Teachers.jsx
@@ -35,7 +35,7 @@ export default class Teachers extends React.Component {
     })
 
     try {
-        const favoriteTeacherJson = await fetch("/api/teacher/true")
+        const favoriteTeacherJson = await fetch("/api/teachers/true")
         const favoriteTeacher = await favoriteTeacherJson.json()
         console.log("favoriteTeacher", favoriteTeacher)
     } catch (err) {
